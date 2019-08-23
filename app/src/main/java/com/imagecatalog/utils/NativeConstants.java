2fa7262bafc1da66adefce6871318027b304ab95
Viplav Kumar Singh <viplavsingh1209@gmail.com>
2019-08-23 09:55:52 +0200

    RandomSHA

--- app/src/main/java/com/imagecatalog/utils/NativeConstants.java
+++ app/src/main/java/com/imagecatalog/utils/NativeConstants.java
@@ -3,5 +3,5 @@ package com.imagecatalog.utils;
 public class NativeConstants {
 
     public static final String BASE_URL = "https://marlove.net/e/mock/v1/items";
-    public static final String[] SHA256 = {"sha256/MUM6ODI6M0Y6Rjc6RUU6MEM6NEE6Q0Y6QkI6NkI6QzU6NzI6QkE6OTg6M0E6OUU6NTM6NDA6NUU6Qzk6QUU6ODc6QkI6QUY6QTI6OUM6QjM6QkI6QkQ6OEI6REE6MzU="};
+    public static final String[] SHA256 = {"sha256/HJfgsfgsfgsdfgdfsg"};
 }
